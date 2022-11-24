668478ecbd0ecf1d8ee145b6ac58429c12e58709
Kushagra <kushagra1607@gmail.com>
2022-11-24 21:38:25 +0530

    .

--- Two Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        a=set()
-        for i,val in enumerate (nums):
-            s=target-nums[i]
-            if s in a:
-                return [nums.index(s),i]
-            else:
-                a.add(val)
